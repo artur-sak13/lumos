c93ecd637cf650c38e8adb83090fb52422fce48e
artur-sak13 <artursak1981@gmail.com>
2016-12-09 05:23:10 -0600

    First commit

---
+++ README.md
@@ -0,0 +1,32 @@
+Project Description:
+======
+
+This is a simple Python shell script which uses voice commands (using the python Speech Recognition library) to toggle lights in a room.
+
+Dependencies:
+======
+Install `command line tools`:
+
+	xcode-select –install
+
+Install `brew`:
+
+	ruby -e “$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)”
+	brew doctor
+
+Install `Python (3.5)`:
+
+	brew install python3
+
+Downloading the Code
+===
+	git clone https://github.com/artur-sak13/lumos.git
+
+Then run,
+
+	pip3 install -r requirements.txt
+
+Running the script
+===
+
+Run `python3.5 lumos.py` in your terminal or `chmod +x lumos.py` to turn the file into an executable and run with `./lumos.py`.
---
+++ lumos.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3.5
+
+import speech_recognition as sr
+import requests
+import pyaudio
+import wave
+
+__author__= "Artur Sak"
+
+'''
+Triggers audio file playback when a key phrase is recognized.
+@param audio_file - a string containing the name of the .wav file to play
+'''
+def play_audio(audio_file):
+	chunk = 1024
+	wf = wave.open(audio_file, 'rb')
+	p = pyaudio.PyAudio()
+
+	stream = p.open(
+		format = p.get_format_from_width(wf.getsampwidth()),
+		channels = wf.getnchannels(),
+		rate = wf.getframerate(),
+		output = True)
+	data = wf.readframes(chunk)
+	count = 0
+
+	while len(data) > 0:
+		stream.write(data)
+		data = wf.readframes(chunk)
+
+	stream.stop_stream()
+	stream.close()
+	p.terminate()
+
+'''
+Activates mic and listens to user commands. Audio is converted to text
+via the Google Speech API and if the response matches a key phrase a request
+is made to an IFTTT event which turns on room lights
+'''
+
+def listen_to_commands():
+	r = sr.Recognizer()
+
+	with sr.Microphone() as source:
+		print("Say something!")
+		audio = r.listen(source)
+	try:
+		cmd = r.recognize_google(audio)
+		cmd = cmd.lower()
+		print("You said: " + cmd)
+		if cmd == "i love lamp":
+			requests.post('https://YOUR_IFFTTT_EVENT_TRIGGER_URL.com')
+			play_audio('ilovelamp.wav')
+		elif cmd == "lumos" or cmd == "nox":
+			requests.post('https://YOUR_IFFTTT_EVENT_TRIGGER_URL.com')
+			play_audio('potter.wav')
+
+	except sr.UnknownValueError:
+		print("Google Speech Recognition could not understand what you said!")
+	except sr.RequestError as e:
+		print("Cannot get results from Google Speech Recognition; {0}".format(e))
+
+def main():
+	listen_to_commands()
+
+if __name__ == '__main__':
+	main()
